54b6f127ef7d500b33ca2f46385c66d11d6cec54
liuh <2210712252@qq.com>
2018-01-10 14:10:26 +0800

    添加了一个请求时添加请求参数的拦截器

--- app/src/main/java/com/liuh/mtoutiao/service/api/ApiRetrofit.java
+++ app/src/main/java/com/liuh/mtoutiao/service/api/ApiRetrofit.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Cache;
+import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -70,7 +71,7 @@ public class ApiRetrofit {
     private Interceptor mHeaderIntercepter = chain -> {
         Request.Builder builder = chain.request().newBuilder();
         builder.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.108 Safari/537.36 2345Explorer/8.0.0.13547");
-        builder.addHeader("Cache-Control", "max-age=0");
+        builder.addHeader("Cache-Control", "max-age=0");//缓存时间为0秒
         builder.addHeader("Upgrade-Insecure-Requests", "1");
         builder.addHeader("X-Requested-With", "XMLHttpRequest");
         builder.addHeader("Cookie", "uuid=\"w:f2e0e469165542f8a3960f67cb354026\"; __tasessionId=4p6q77g6q1479458262778; csrftoken=7de2dd812d513441f85cf8272f015ce5; tt_webid=36385357187");
@@ -98,6 +99,20 @@ public class ApiRetrofit {
                 .build();
     };
 
+    /**
+     * 对旧的oldRequest做了一堆处理，加入了一些通用的统计参数，包装成生成了一个新的newRequest，然后调用chain.proceed方法
+     */
+    private Interceptor mNetStatisticsInterceptor = (Interceptor.Chain chain) -> {
+        Request request = chain.request();
+        HttpUrl httpUrl = request.url();
+        HttpUrl.Builder urlBuilder = httpUrl.newBuilder();
+        if (httpUrl.queryParameter("app_version") == null) {
+//            urlBuilder.addQueryParameter("app_version", BaseConfig.versionName);
+            urlBuilder.addQueryParameter("app_version", "3.1");
+        }
+        // 用chain.request()构造一个新的传入统计参数的request，作为参数调用chain.proceed
+        return chain.proceed(request.newBuilder().url(urlBuilder.build()).build());
+    };
 
     public ApiService getmApiService() {
         return mApiService;
